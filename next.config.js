56f4a180ac123e7184eaec99174413b1719b6dd9
whiteindian21 <wendelandrady10@gmail.com>
2026-04-19 22:35:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverExternalPackages: ['openai'], // prevents Next.js from bundling openai
+};
 
-export default nextConfig
+export default nextConfig;
